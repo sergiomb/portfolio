b40a42d672e1f868ed886b31166185b46d7784cf
Sergio Mendez <sergio@wunderdigital.com.br>
2023-12-21 11:38:28 -0300

    Auto-generated commit [ci skip]

---
+++ assets/bootstrap/script.store.ts
@@ -0,0 +1,10 @@
+
+interface Scripts {
+  name: string
+  src: string
+  }
+
+export const ScriptStore: Scripts[] = [
+  {name: 'app', src: '../bootstrap/app.js'},
+  {name: 'settings', src: '../bootstrap/settings.js'}
+  ]
